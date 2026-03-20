6a776e1c9f0f8a94d357325ca2189a621df25bf1
Sivakaran <Sivakaran@DESKTOP-AUHSFF3>
2026-03-20 18:10:20 +0000

    azure database execution

---
+++ AuthApi/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using AuthApi.Models;
+using Microsoft.AspNetCore.Identity;
+
+public static class DbInitializer
+{
+	public static async Task SeedAsync(IServiceProvider serviceProvider)
+	{
+		using var scope = serviceProvider.CreateScope();
+		var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+		var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+		// Create Admin Role
+		if (!await roleManager.RoleExistsAsync("Admin"))
+		{
+			await roleManager.CreateAsync(new IdentityRole("Admin"));
+		}
+
+		// Create Admin User
+		var adminEmail = "admin@gmail.com";
+		var adminPassword = "Admin@123";
+
+		var adminUser = await userManager.FindByEmailAsync(adminEmail);
+		if (adminUser == null)
+		{
+			adminUser = new ApplicationUser
+			{
+				UserName = adminEmail,
+				Email = adminEmail,
+				EmailConfirmed = true
+			};
+
+			await userManager.CreateAsync(adminUser, adminPassword);
+			await userManager.AddToRoleAsync(adminUser, "Admin");
+		}
+	}
+}
